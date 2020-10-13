b5efcdb005166841b32b22bf8b3b5e1bd173a33c
Dipak Parmar <hi@dipak.tech>
2020-10-13 02:57:37 -0700

    Set some default values for wp-comfig if not provided in environment variables

--- wp-config.php
+++ wp-config.php
@@ -4,8 +4,6 @@ if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROT
         $_SERVER['HTTPS'] = 'on';
 }
 
-define('WP_AUTO_UPDATE_CORE', false);
-
 $table_prefix  = getenv('TABLE_PREFIX') ?: 'wp_';
 if(getenv('TABLE_PREFIX') == 'null') {
 	$table_prefix = '';
@@ -16,6 +14,18 @@ foreach ($_ENV as $key => $value) {
     if (!defined($capitalized)) {
         define($capitalized, $value);
     }
+}
+
+if(!defined('WP_AUTO_UPDATE_CORE')) {
+    define('WP_AUTO_UPDATE_CORE', false);
+}
+
+if(!defined('CORE_UPGRADE_SKIP_NEW_BUNDLED')) {
+    define('CORE_UPGRADE_SKIP_NEW_BUNDLED', true);
+}
+
+if(!defined('WPLANG')) {
+    define('WPLANG', 'en_CA');
 }
 
 if (!defined('ABSPATH')) {
